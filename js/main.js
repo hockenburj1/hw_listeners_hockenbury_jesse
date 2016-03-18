c0ffdc50dfb24dacf209760d89826dac1e33987c
Jesse Hockenbury <hockenburj1@nku.edu>
2016-03-18 18:50:42 -0400

    added buttons, added clear and danger listeners, made css adjustments for looks

--- js/main.js
+++ js/main.js
@@ -6,4 +6,24 @@ function calculateCTotal( e ) {
     total.value = parseInt( number1 ) + parseInt( number2 );
 }
 
+function clearFields() {
+    document.getElementsByName( 'number1' )[ 0 ].value = '';
+    document.getElementsByName( 'number2' )[ 0 ].value = '';
+    document.getElementsByName( 'total' )[ 0 ].value = '';
+}
+
+function dangerMessage() {
+    dangerWarning = document.createElement( 'p' );
+    dangerWarningText = document.createTextNode( 'You were warned not to go near there!' );
+    dangerWarning.appendChild( dangerWarningText );
+    document.getElementsByClassName( 'container' )[ 0 ].appendChild( dangerWarning );
+}
+
+// Upon the second field change calculate total
 document.getElementsByName( 'number2' )[ 0 ].addEventListener( 'change', calculateCTotal );
+
+// Clear all fields upon click of clear button
+document.getElementById( 'clear' ).addEventListener( 'click', clearFields );
+
+// Clear all fields upon click of clear button
+document.getElementById( 'danger' ).addEventListener( 'mouseover', dangerMessage );
